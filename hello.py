ae414cf42ad16ef0d7d4f62e958642a42ac801d0
Julian Tormes <juliants10@gmail.com>
2024-02-26 02:34:52 -0300

    Modified hello world with better comments

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print("Hello world")
+def welcoming():
+    print("Hello world") #* greetings
+    print("Danger world")#! alert
+    print("Goodbye world") #? question
+    print("H") #TODO: incopmlete phrase
+    
+def main():
+    welcoming()
+if __name__ == "__main__":
+    main()
